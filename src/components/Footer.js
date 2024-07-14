320b08f6151defe6e9517a97c1afa6cb03aa53c5
Dawid Kuptel <105428376+Dawka86@users.noreply.github.com>
2024-07-14 17:37:38 +0000

    added full year update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 export default function Footer() {
+  function getFullYear() {
+    const d = new Date();
+    const year = d.getFullYear();
+    return year;
+  }
+
   return (
     <div class="container-fluid common_container">
       <footer class="d-flex flex-wrap justify-content-between align-items-center py-3  border-top">
@@ -10,7 +16,8 @@ export default function Footer() {
             class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1"
           ></a>
           <span class="mb-3 mb-md-0 text-body-secondary">
-            © 2024 Company, Inc
+            © <span className="tittle_font_footer"> LectraMart </span>
+            {getFullYear()}
           </span>
         </div>
 
