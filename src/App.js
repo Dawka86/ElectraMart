93afca9ca82832d44efb30929567e455e7969435
Dawid Kuptel <105428376+Dawka86@users.noreply.github.com>
2024-07-14 17:36:52 +0000

    added routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import "./styles.css";
 import Jumbotron from "./components/Jumbotron";
 import Footer from "./components/Footer";
+import Sign from "./components/Sign";
 
 export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <Jumbotron />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <HeaderWrapper />
+        <Routes>
+          <Route exact path="/" element={<Jumbotron />} />
+          <Route path="/sign" element={<Sign />} />
+          {/* Możesz dodać więcej tras tutaj */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
+  function HeaderWrapper() {
+    const location = useLocation();
+    console.log(location.pathname);
+    return location.pathname !== "/Sign" ? <Header /> : null;
+  }
 }
