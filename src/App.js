a8b9ee665411c89387bacd36db0fa48a7eb4c869
Dawid Kuptel <105428376+Dawka86@users.noreply.github.com>
2024-07-18 17:24:57 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -10,17 +10,22 @@ import "./styles.css";
 import Jumbotron from "./components/Jumbotron";
 import Footer from "./components/Footer";
 import Sign from "./components/Sign";
+import IconGrid from "./components/IconGrid";
+import Cart from "./components/Cart";
+import CartButton from "./components/CartButton";
 
 export default function App() {
   return (
     <Router>
       <div className="App">
         <HeaderWrapper />
+        <CartButton />
         <Routes>
           <Route exact path="/" element={<Jumbotron />} />
           <Route path="/sign" element={<Sign />} />
-          {/* Możesz dodać więcej tras tutaj */}
         </Routes>
+        <CartWrapper />
+        <IconGritWrapper />
         <Footer />
       </div>
     </Router>
@@ -29,5 +34,15 @@ export default function App() {
     const location = useLocation();
     console.log(location.pathname);
     return location.pathname !== "/Sign" ? <Header /> : null;
+  }
+  function IconGritWrapper() {
+    const location = useLocation();
+    console.log(location.pathname);
+    return location.pathname !== "/Sign" ? <IconGrid /> : null;
+  }
+  function CartWrapper() {
+    const location = useLocation();
+    console.log(location.pathname);
+    return location.pathname !== "/Sign" ? <Cart /> : null;
   }
 }
