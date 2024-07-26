9a6fdbb8d9bbd93b8eed4ec4b61513e9dcbe15c0
Dawid Kuptel <105428376+Dawka86@users.noreply.github.com>
2024-07-26 17:52:13 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/Header";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
-import "./styles.css";
-import Jumbotron from "./components/Jumbotron";
-import Footer from "./components/Footer";
-import Sign from "./components/Sign";
-import IconGrid from "./components/IconGrid";
-import Cart from "./components/Cart";
-import CartButton from "./components/CartButton";
-import IconGridProm from "./components/IconGridProm";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles.css";
+
+import Sign from "./components/sign/Sign";
+import Main from "./components/home/Main";
+import Footer from "./components/home/Footer";
 export default function App() {
   return (
     <Router>
       <div className="App">
-        <HeaderWrapper />
-        <CartButton />
         <Routes>
-          <Route exact path="/" element={<Jumbotron />} />
+          <Route exact path="/" element={<Main />} />
           <Route path="/sign" element={<Sign />} />
-        </Routes>
-        <CartWrapper />
-        <IconGridProm />
-        <IconGritWrapper />
-
+        </Routes>{" "}
         <Footer />
-      </div>
+      </div>{" "}
     </Router>
   );
-  function HeaderWrapper() {
-    const location = useLocation();
-    console.log(location.pathname);
-    return location.pathname !== "/Sign" ? <Header /> : null;
-  }
-  function IconGritWrapper() {
-    const location = useLocation();
-    console.log(location.pathname);
-    return location.pathname !== "/Sign" ? <IconGrid /> : null;
-  }
-  function CartWrapper() {
-    const location = useLocation();
-    console.log(location.pathname);
-    return location.pathname !== "/Sign" ? <Cart /> : null;
-  }
 }
