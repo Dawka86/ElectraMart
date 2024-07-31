42f2d3e0199a4ff263ad173dce34cbdc76724178
Dawid Kuptel <105428376+Dawka86@users.noreply.github.com>
2024-07-31 16:18:35 +0000

    i added new route path.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles.css";
 
-import Sign from "./components/sign/Sign";
-import Main from "./components/home/Main";
-import Footer from "./components/home/Footer";
+import Sign from "./components/Sign/Sign";
+import Main from "./components/Home/Main";
+import Footer from "./components/Home/Footer";
+import ProductsPage from "./components/ProductPage/productsPage";
 export default function App() {
   return (
     <Router>
@@ -13,6 +14,7 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={<Main />} />
           <Route path="/sign" element={<Sign />} />
+          <Route path="/product" element={<ProductsPage />} />
         </Routes>{" "}
         <Footer />
       </div>{" "}
