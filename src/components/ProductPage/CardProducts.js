fce8c91d84031ca3b4493ac878e72c07385e3692
Dawid Kuptel <105428376+Dawka86@users.noreply.github.com>
2024-08-01 17:02:57 +0000

    update cardProducts component

--- src/components/ProductPage/CardProducts.js
+++ src/components/ProductPage/CardProducts.js
@@ -1,17 +1,20 @@
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/esm/Button";
 
-export default function CardProducts() {
+export default function CardProducts({ name, img }) {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="" />
+    <Card style={{ width: "auto" }}>
+      <Card.Img variant="top" src={img} style={{ height: "45vh" }} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title className="cart_tittle">{name}</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
+        <Button className="buy_button" variant="sm">
+          Price
+        </Button>
       </Card.Body>
     </Card>
   );
