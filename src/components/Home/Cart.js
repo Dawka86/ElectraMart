a6c8968661f4ebea83bb0b6400120fbbdd2ff918
Dawid Kuptel <105428376+Dawka86@users.noreply.github.com>
2024-08-07 17:01:14 +0000

    i added new component

---
+++ src/components/Home/Cart.js
@@ -0,0 +1,32 @@
+import Products from "./Products";
+import headphones from "../../assets/images/headphone.jpg";
+import watch from "../../assets/images/watch.jpg";
+import computer from "../../assets/images/computer.jpg";
+
+export default function Cart() {
+  return (
+    <div className="container">
+      <h3 className="pb-2 border-bottom text-start">Most popular products</h3>
+      <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <Products
+          img={headphones}
+          name="headphone"
+          price="24.99 £"
+          description="Sony WH-CH520 Wireless"
+        />
+        <Products
+          img={watch}
+          name="watch"
+          price="140.50 £"
+          description="Samsung Galaxy Watch7"
+        />
+        <Products
+          img={computer}
+          name="computer"
+          price="350 £"
+          description="Dell Inspiron 14 5440 Laptop 14"
+        />
+      </div>
+    </div>
+  );
+}
