af871b24f5a0e1296f2597b32e302f0790e79baa
Dawid Kuptel <105428376+Dawka86@users.noreply.github.com>
2024-08-22 18:20:46 +0000

    update

--- src/components/ProductPage/productsPage.js
+++ src/components/ProductPage/productsPage.js
@@ -1,24 +1,131 @@
 import Accordion from "react-bootstrap/Accordion";
 import CardProducts from "./CardProducts";
+import tv1 from "../../assets/images/tv1.jpg";
+import tv2 from "../../assets/images/tv2.jpg";
+import tv3 from "../../assets/images/tv3.jpg";
+import PlayS5 from "../../assets/images/PlayS5.jpg";
+import xboxX from "../../assets/images/xboxX.jpg";
+import nintendo from "../../assets/images/nintendo.jpg";
+import { useState } from "react";
+import OverlayProducts from "./Overlay";
+import CartMessage from "./CartMessage";
 
 export default function ProductsPage() {
+  const [items, setItems] = useState([]); // Tablica produktow
+  const [show, setShow] = useState(false);
+
+  function showMessage() {
+    setShow(true);
+  }
+
+  function hiddenMessage() {
+    setShow(false);
+  }
+
+  function addItem(item) {
+    setItems((prevItems) => {
+      return [...prevItems, item];
+    });
+    showMessage();
+  }
+
+  function deleteProduct(indexToRemove) {
+    setItems((prevItems) =>
+      prevItems.filter((_, index) => index !== indexToRemove),
+    );
+  }
+
+  const totalPrice = items.reduce((sum, items) => sum + items.price, 0);
+
   return (
-    <Accordion defaultActiveKey={["0"]} alwaysOpen>
+    <Accordion defaultActiveKey="0" alwaysOpen>
       <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <CartMessage show={show} hiddenMessage={hiddenMessage} />
+        <div>
+          <OverlayProducts
+            items={items}
+            totalPrice={totalPrice}
+            removeFromCart={deleteProduct}
+          />
+        </div>
+        <Accordion.Header>TV set</Accordion.Header>
         <Accordion.Body>
           <div className="container-fluid">
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
               <div className="col">
-                {" "}
+                <CardProducts
+                  name="SAMSUNG QE"
+                  img={tv1}
+                  price={850}
+                  addToCart={() => addItem({ name: "SAMSUNG QE", price: 850 })}
+                />
+              </div>
+              <div className="col">
+                <CardProducts
+                  name="SONY BRAVIA 8 K"
+                  img={tv2}
+                  price={750}
+                  addToCart={() =>
+                    addItem({ name: "SONY BRAVIA 8 K", price: 750 })
+                  }
+                />
+              </div>
+              <div className="col">
+                <CardProducts
+                  name="OLED evo LG"
+                  img={tv3}
+                  price={550}
+                  addToCart={() => addItem({ name: "OLED evo LG", price: 550 })}
+                />
+              </div>
+            </div>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Consoles</Accordion.Header>
+        <Accordion.Body>
+          <div className="container-fluid">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <CardProducts
+                  name="Playstation 5"
+                  img={PlayS5}
+                  price={400}
+                  addToCart={() =>
+                    addItem({ name: "Playstation 5", price: 400 })
+                  }
+                />
+              </div>
+              <div className="col">
+                <CardProducts
+                  name="Xbox X"
+                  img={xboxX}
+                  price={400}
+                  addToCart={() => addItem({ name: "Xbox X", price: 400 })}
+                />
+              </div>
+              <div className="col">
+                <CardProducts
+                  name="Nintendo"
+                  img={nintendo}
+                  price={200}
+                  addToCart={() => addItem({ name: "Nintendo", price: 200 })}
+                />
+              </div>
+            </div>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Laptops</Accordion.Header>
+        <Accordion.Body>
+          <div className="container-fluid">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
                 <CardProducts />
               </div>
               <div className="col">
-                {" "}
-                <CardProducts />
-              </div>
-              <div className="col">
-                {" "}
                 <CardProducts />
               </div>
               <div className="col">
@@ -28,100 +135,94 @@ export default function ProductsPage() {
           </div>
         </Accordion.Body>
       </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
       <Accordion.Item eventKey="3">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>PC</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <div className="container-fluid">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+            </div>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="4">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>Keyboard</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <div className="container-fluid">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+            </div>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="5">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>Headphones</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <div className="container-fluid">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+            </div>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="6">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>Cameras</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <div className="container-fluid">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+            </div>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="7">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header> Mobile Phones</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="8">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <div className="container-fluid">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+              <div className="col">
+                <CardProducts />
+              </div>
+            </div>
+          </div>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
